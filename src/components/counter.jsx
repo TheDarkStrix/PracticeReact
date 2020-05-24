b96b1d71553583512aab335f7e314544d73d5ca0
Manoj P <thedarkstrix@gmail.com>
2020-05-24 18:47:38 +0530

    Using states

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      //   <React.Fragment>
+      //     <span>Hello World</h1>
+      //     <button>Increment</button>
+      //   </React.Fragment>
+      <div>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
   }
 }
 
