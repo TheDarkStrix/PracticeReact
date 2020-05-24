cc40992e698997a001ff7832ff18d59bbff29f46
Manoj P <thedarkstrix@gmail.com>
2020-05-24 19:45:24 +0530

    Binding using constructor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,23 +6,37 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  handleIncrement() {
+    console.log("Increment Clicked", this);
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
 
